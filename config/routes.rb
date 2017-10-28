0121b8565855bcd479c65efa2a32ffde0ea34192
Nix <yiunixon@gmail.com>
2017-10-28 14:38:28 -0700

    Wrte Auth Backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  namespace :api do
-    get 'user/create'
+  namespace :api, defaults: { format: :json } do
+    resource :user, only: %i(create)
+    resource :session, only: %i(create destroy show)
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
