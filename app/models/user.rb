7db5ad270d5d0a1faf56095f073b57cfbe41a10a
Nix <yiunixon@gmail.com>
2017-10-28 13:28:45 -0700

    Write user conttoller

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
 
   after_initialize :ensure_session_token
 
+
   def self.find_by_credentials(username, password)
     user = User.find_by(username: username)
     return nil unless user
