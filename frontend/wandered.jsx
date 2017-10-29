7bd63942a7f43b36df6d7e147400baf829c981f1
Nix <yiunixon@gmail.com>
2017-10-29 13:33:41 -0700

    Write react router and session components

--- frontend/wandered.jsx
+++ frontend/wandered.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {login, logout, signup} from './util/session_api_util';
+import Root from './components/root';
+import configureStore from './store/store';
 
 window.login = login;
 window.logout = logout;
 window.signup = signup;
 
+
+
 document.addEventListener('DOMContentLoaded', () => {
+  let store;
+  if (window.currentUser) {
+    const preloadedState = { session: { currentUser: window.currentUser } };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
+  }
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Welcome to Wandered</h1>, root);
+  ReactDOM.render(<Root store={store} />, root);
 });
