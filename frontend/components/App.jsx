009da25b6bee3ab191a341490e2cc2f1a3191795
Nix <yiunixon@gmail.com>
2017-10-29 21:36:18 -0700

    Bootstrap user

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -9,11 +9,20 @@ import {
 } from 'react-router-dom';
 
 import GreetingContainer from './greeting/greeting_container';
+import SessionFormContainer from './session_form/session_form_container';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
 const App = () => (
   <div>
-    <h1>Wandered</h1>
-    <GreetingContainer />
+    <header>
+      <h1>Wandered</h1>
+      <GreetingContainer />
+    </header>
+
+    <Switch>
+      <AuthRoute path="/login" component={SessionFormContainer} />
+      <AuthRoute path="/signup" component={SessionFormContainer} />
+    </Switch>
   </div>
 );
 
--- frontend/wandered.jsx
+++ frontend/wandered.jsx
@@ -11,13 +11,14 @@ window.signup = signup;
 
 
 document.addEventListener('DOMContentLoaded', () => {
-  const store = configureStore();
-
-  // TESTING START
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
-  // TESTING END
-
+  let store;
+  if (window.currentUser) {
+    const preloadedState = { session: { currentUser: window.currentUser } };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store} />, root);
 });
